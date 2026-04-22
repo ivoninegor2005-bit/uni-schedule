80b00d9fd3b3d04dc21463de11fc1a9d244c8329
ivoninegor2005-bit <ivoninegor2005@gmail.com>
2026-04-22 17:13:07 +0500

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,52 +1,17 @@
-import requests
-import os
+from icalendar import Calendar, Event
+from datetime import datetime
 
-URL = "https://uspu.ru/education/eios/schedule/calendar/calendar_filter/calendar_БХ-2331_22.04.2026%2012%3A00%3A00_13.05.2026%2012%3A00%3A00.ics"
+cal = Calendar()
+cal.add("prodid", "-//Uni Schedule//RU")
+cal.add("version", "2.0")
 
-HEADERS = {
-    "User-Agent": "Mozilla/5.0"
-}
+event = Event()
+event.add("uid", "1")
+event.add("dtstart", datetime(2026, 4, 22, 12, 0, 0))
+event.add("dtend", datetime(2026, 4, 22, 13, 35, 0))
+event.add("summary", "Методика обучения биологии")
 
-def download():
-    try:
-        r = requests.get(URL, headers=HEADERS, timeout=30)
-        r.raise_for_status()
-        return r.text
-    except Exception as e:
-        print("Download failed:", e)
-        return None
+cal.add_component(event)
 
-def is_valid(ics):
-    return ics and "BEGIN:VEVENT" in ics
-
-def load_old():
-    if os.path.exists("calendar.ics"):
-        return open("calendar.ics", "r", encoding="utf-8").read()
-    return None
-
-def save(data):
-    with open("calendar.ics", "w", encoding="utf-8") as f:
-        f.write(data)
-
-def main():
-    print("Downloading ICS...")
-
-    new_ics = download()
-
-    if is_valid(new_ics):
-        print("Valid ICS → saving")
-        save(new_ics)
-        return
-
-    print("Invalid ICS → fallback")
-
-    old = load_old()
-
-    if is_valid(old):
-        print("Using previous version")
-        save(old)
-    else:
-        print("Empty calendar fallback")
-        save("BEGIN:VCALENDAR\nVERSION:2.0\nEND:VCALENDAR\n")
-
-main()
+with open("calendar.ics", "wb") as f:
+    f.write(cal.to_ical())
